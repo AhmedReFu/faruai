95cbc2d1a5678f7a4fe95374c841a8e572e5e5f1
AhmedReFu <mdnazmulkhanfeni@gmil.com>
2025-07-06 00:48:35 +0600

    04:-authentication-ui

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+
+import { SignUpView } from "@/app/modules/auth/ui/views/sign-up-view";
+
+const page = () => {
+
+  return <SignUpView/>
+}
+
+export default page;
+
+//http://localhost:3000/sign-in
